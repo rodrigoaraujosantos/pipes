3b688e335c31ca835b107620b4e37cbe9211c953
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-10-25 14:42:08 -0300

    update Regex CPF

--- src/app/formata-cpf.pipe.ts
+++ src/app/formata-cpf.pipe.ts
@@ -5,24 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormataCpfPipe implements PipeTransform {
 
-  transform(value: string|number,
-    
-    ocultarAlgunsValores: boolean = false): string  {
-    
-      let resultado = value + '';
-
-    resultado = resultado
-
-    .padStart(11, '0')
-    .substr(0, 11)
-    .replace(/[^0-9]/, '')
-    .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
-
-    if (ocultarAlgunsValores) {
-      resultado = 'xxx.' + resultado.substr(4, 7) + '-xx';
-    }
-
-    return resultado
+  transform(value: string): string {
+    const cpf = value.replace(/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/, '$1.$2.$3-$4')
+    return cpf
   }
 
 }
